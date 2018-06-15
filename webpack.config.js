d4dd21cb48a3cb9eb29a3e297b7d71d8960e8305
akari0624 <akari0624@hotmail.com>
2018-06-15 12:01:17 +0800

    use babel-polyfill, in order to use async/await

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require('path');
 const HtmlWebPackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  entry: ['./src/index.js'],
+  entry: ['babel-polyfill', './src/index.js'],
   output: {
     path: __dirname,
     publicPath: '/',
